edb12236dc483eb458001030b60fe21bdb965aea
Nordine Bittich <nordine@bittich.be>
2023-06-10 11:05:00 +0200

    bugfix polyglot error

--- send-reminder-pay-expense.js
+++ send-reminder-pay-expense.js
@@ -3,6 +3,17 @@ const SCRIPT_NOTIFY = "SCRIPT_NOTIFY";
 const List = Java.type('java.util.List');
 const ArrayList = Java.type('java.util.ArrayList');
 
+// temporary workaround solution for  
+// org.graalvm.polyglot.PolyglotException: Unsupported operation identifier 'toArray' and  
+// object '["d44d35ef-52b2-45f6-b021-768918bf4922"]'(language: JavaScript, type: Array). 
+// Identifier is not executable or instantiable.
+function toArray(jsArray) {
+  const list = new ArrayList();
+  for (const jsObj of jsArray) {
+    list.add(jsObj);
+  }
+  return list;
+}
 class Script {
   get id() {
     return "33fb12f0-800e-400d-b83f-5f6cf6c15c22";
@@ -27,7 +38,7 @@ class Script {
       const personalEmail = personalInfo.getOrganizationEmailAddress();
       if (personalEmail) {
         let subject = "Reminder: expense(s) not paid";
-        const expenses = feeService.findAll(event.expenseIds);
+        const expenses = feeService.findAll(toArray(event.expenseIds));
 
         if (!expenses.isEmpty()) {
           logger.info("Send email with reminder");
